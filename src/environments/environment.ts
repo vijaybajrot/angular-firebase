a76111309e0f5484e3f337a2372cf4c4d7d0d20a
Vijay Bajrot <vijaybajrot@gmail.com>
2019-10-06 21:52:04 +0530

    config updated

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,18 +1,11 @@
+import config from '../../config-override'
 // This file can be replaced during build by using the `fileReplacements` array.
 // `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
   production: false,
-  firebase: {
-    apiKey: "AIzaSyBKk9PoZY87A-7Yfk4PIRjfcDjcaro4CCM",
-    authDomain: "ng-firebase-6bf04.firebaseapp.com",
-    databaseURL: "https://ng-firebase-6bf04.firebaseio.com",
-    projectId: "ng-firebase-6bf04",
-    storageBucket: "ng-firebase-6bf04.appspot.com",
-    messagingSenderId: "314884373972",
-    appId: "1:314884373972:web:79094c8e72088464152a41"
-  }
+  firebase: config.firebase
 };
 
 /*
